405d8094abd6fa074e3393b0149ca82a11b890d2
Mr. Happy Nice <themrhappynice@gmail.com>
2025-11-22 17:28:34 -0600

    Update get-bin.sh

--- get-bin.sh
+++ get-bin.sh
@@ -1,1 +1,1 @@
-curl -L -o bplus-launcher https://github.com/mrhappynice/bplus-launcher/releases/download/v0.1.0/bplus-launcher && chmod +x bplus-launcher
+curl -L -o bplus-launcher https://github.com/mrhappynice/bplus-launcher/releases/download/v0.2.5/bplus-launcher && chmod +x bplus-launcher
